c5639b1c5a581733f10f8adab36f0957fd86aaeb
Ulises Barrionuevo <ulitito54@hotmail.es>
2023-08-19 11:00:20 +0200

    new

--- src/components/UrlShortener.vue
+++ src/components/UrlShortener.vue
@@ -1,42 +1,60 @@
-<template >
-  <div class="d-flex align-items-center justify-content-center min-vh-100  bg-black ">
-    <div class="flex flex-col items-center p-8 rounded-lg shadow-md w-80 text-center bg-black run ">
-   
-    <div class="div-principal"> 
-      <div class="mb-6 ">
-      
-       
+<template>
+  <div class="d-flex align-items-center justify-content-center min-vh-100 bg-black">
+    <div class="flex flex-col items-center p-4 rounded-lg shadow-md w-90 text-center bg-black run">
+      <div class="mb-4">
         <a href="https://github.com/Ubarrionuevo/G.Password" target="_blank" class="bg-red-500 hover:bg-gray-700 text-white font-bold py-2 px-4 custom-link">
           GitHub
-      </a>
-      <a href="https://twitter.com/TittoDev_" target="_blank" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4  ml-4 custom-link">
-        Twitter
-      </a>
+        </a>
+        <a href="https://twitter.com/TittoDev_" target="_blank" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-4 custom-link">
+          Twitter
+        </a>
       </div>
-      <h2 class="text-xl font-semibold mb-4 text-white mt-5">GPassword</h2>
+      <h2 class="text-xl font-semibold mb-2 text-white mt-5">GPassword</h2>
       <p class="text-white">Random Password Generator</p>
-      <button @click="generarContraseña" class=" text-white font-bold py-2 px-4  mb-4 bg-black click">
-       Generator
+      <button @click="generarContraseña" class="text-white font-bold py-2 px-4 mb-2 bg-black click">
+        Generator
       </button>
-      <button @click="copiarContraseña" class="bg-black text-white font-bold py-2 px-4  mt-2 click ">
+      <button @click="copiarContraseña" class="bg-black text-white font-bold py-2 px-4 click">
         Copy
       </button>
-    
-      <p class="text-white" >{{ contraseñaGenerada }}</p>
+      <p class="text-white">{{ contraseñaGenerada }}</p>
       <h6 v-if="copied" style="color: green;">Password copied successfully!</h6>
-     
     </div>
-   
-      
-    </div>
-
   </div>
-
-
 </template>
+<!-- 
+<template>
+  <div class="d-flex align-items-center justify-content-center min-vh-100 bg-black">
+    <div class="flex flex-col items-center p-4 rounded-lg shadow-md w-90 text-center bg-black run">
+      <div class="mb-4">
+        <a href="https://github.com/Ubarrionuevo/G.Password" target="_blank" class="bg-red-500 hover:bg-gray-700 text-white font-bold py-2 px-4 custom-link">
+          GitHub
+        </a>
+        <a href="https://twitter.com/TittoDev_" target="_blank" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 ml-4 custom-link">
+          Twitter
+        </a>
+      </div>
+      <h2 class="text-xl font-semibold mb-2 text-white mt-5">GPassword</h2>
+      <p class="text-white">Random Password Generator</p>
+      <button @click="generarContraseña" class="text-white font-bold py-2 px-4 mb-2 bg-black click">
+        Generator
+      </button>
+      <button @click="copiarContraseña" class="bg-black text-white font-bold py-2 px-4 click">
+        Copy
+      </button>
+      <p class="text-white">{{ contraseñaGenerada }}</p>
+      <h6 v-if="copied" style="color: green;">Password copied successfully!</h6>
+    </div>
+  </div>
+</template> -->
+
+<!-- Resto del código -->
+
+
+
 
 <script>
-
+import cryptoRandomString from 'crypto-random-string';
 
 export default {
   data() {
@@ -48,20 +66,15 @@ export default {
     };
   },
   methods: {
-   
-     generarContraseña() {
-       const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}|:<>?~-';
-       const longitudMinima = 16;
-       let contraseña = '';
-
-     while (contraseña.length < longitudMinima) {
-         const randomIndex = Math.floor(Math.random() * caracteres.length);
-         contraseña += caracteres.charAt(randomIndex);
-       }
-
-      this.contraseñaGenerada = contraseña;
-      this.copied = false;
-     },
+    generarContraseña() {
+      const length = 16; // Longitud de la contraseña
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+{}|:<>?~-'; // Caracteres posibles
+    const contraseña = cryptoRandomString({ length, characters });
+  
+    this.contraseñaGenerada = contraseña;
+    this.copied = false;
+  },
+    
   
 
     copiarContraseña() {
@@ -83,13 +96,7 @@ export default {
   border: 1px solid gray;
   border-radius: 30px;
 }
-.div-principal{
-  border: 1px solid gray  ; 
-  border-radius: 60px;
-  padding: 100px;
-  height: 440px; /* Establece una altura fija para el div principal */
-  
-}
+
 .custom-link{
 
   text-decoration: none;
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
+
+
 // import { library } from '@fortawesome/fontawesome-svg-core';
 // import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 // import { faIconName } from '@fortawesome/free-solid-svg-icons'; // Reemplaza "IconName" con el nombre del icono que deseas
